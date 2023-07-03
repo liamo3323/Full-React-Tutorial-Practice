81236c05768e940344d5b5d2283ad91a516c849a
Liam <liam3323@gmail.com>
2023-07-03 23:06:36 +0100

    Finished Ep5 multi components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import './App.css';
+import './App.css'
+import Navbar from './Navbar';
+import Home from './Home'
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = 'http://www.youtube.com'
-
-
   return (  // functions always need to return something usually JSX template
     <div className="App">
-
+      <Navbar />
       <div className="content">
-        <h1> {title} </h1>
-        <p> Num of Likes: {likes}</p>
-
-        <p>{ 10 }</p>
-        <p>{"Yo"}</p>
-        <p>{ [1,2,3,4,5]}</p>
-        <p> {Math.random() * 99}</p>
-
-        <a href={link}> youtube website</a>
+        <Home />
       </div>
-
     </div>
   );
 }
