1e36314de9463a494ea1165671975f8353136dab
Liam <liam3323@gmail.com>
2023-07-03 22:55:27 +0100

    finished episode 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = 'http://www.youtube.com'
+
+
+  return (  // functions always need to return something usually JSX template
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="content">
+        <h1> {title} </h1>
+        <p> Num of Likes: {likes}</p>
+
+        <p>{ 10 }</p>
+        <p>{"Yo"}</p>
+        <p>{ [1,2,3,4,5]}</p>
+        <p> {Math.random() * 99}</p>
+
+        <a href={link}> youtube website</a>
+      </div>
+
     </div>
   );
 }
 
-export default App;
+export default App; // we always want to export component so that they can be used on the DOM I think
