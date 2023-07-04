c8984efaae41ae3d9ec8be899a3256b67d7263ab
Liam <liam3323@gmail.com>
2023-07-04 23:35:36 +0100

    Finished EP10 Lists and some CSS

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,20 @@
 import { useState } from 'react'
 
 const Home = () => {
-    // let name = 'mario';
-    const [name, setName] = useState('mario'); 
-    const [age, setAge] = useState(20);
-
-    const handleClick = () => {
-        setName('luigi')
-        setAge(2)
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new Website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev tips', body: 'lorem ipsum...', author: 'peach', id: 3 }
+    ])
 
     return ( 
         <div className="home">
-            <h2>Homepage!</h2>
-            <p>{ name } is { age } years old!</p>
-            <button onClick={() => {
-                handleClick('mario')
-            }}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key = {blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
