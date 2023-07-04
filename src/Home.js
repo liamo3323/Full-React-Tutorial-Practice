f9c4f190f71f164a0f7dfe89bab300a81c25b5ad
Liam <liam3323@gmail.com>
2023-07-04 23:13:47 +0100

    Finished EP7 buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+
+    const handleClick = (name) => {
+        console.log('Hello ' + name)
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage!</h2>
+            <button onClick={() => {
+                handleClick('mario')
+            }}>Click me</button>
         </div>
      );
 }
