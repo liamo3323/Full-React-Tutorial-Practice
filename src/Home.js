2a0f23a17725778ad684be05f9da050b8a9ceb78
Liam <liam3323@gmail.com>
2023-07-04 23:20:53 +0100

    Finished EP8 Hooks

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
-const Home = () => {
+import { useState } from 'react'
 
-    const handleClick = (name) => {
-        console.log('Hello ' + name)
+const Home = () => {
+    // let name = 'mario';
+    const [name, setName] = useState('mario'); 
+    const [age, setAge] = useState(20);
+
+    const handleClick = () => {
+        setName('luigi')
+        setAge(2)
     }
 
     return ( 
         <div className="home">
             <h2>Homepage!</h2>
+            <p>{ name } is { age } years old!</p>
             <button onClick={() => {
                 handleClick('mario')
             }}>Click me</button>
